3e78239608bcf496e8870a9c209a725660035502
futurewan <393192789@qq.com>
2023-05-18 22:10:43 +0800

    feat: generics

--- 05_oop/15_generics_function.dart
+++ 05_oop/15_generics_function.dart
@@ -1,10 +1,16 @@
 /// 泛型
 ///
 
-T getData<T>(T value) {
+// T getData<T>(T value) {
+//   return value;
+// }
+
+//只约定参数类型，不约定函数返回值类型
+getData<T>(T value) {
   return value;
 }
 
 void main() {
-  print(getData('hello'));
+  print(getData<int>(90));
+  print(getData<String>('hello'));
 }
